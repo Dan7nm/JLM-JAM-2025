c707d265b6f3ae76dc10c75c6df41901d8498b57
Dan Nemirovski <dan7.nm@gmail.com>
2025-01-02 12:17:06 +0200

    Fixed Hill Check

--- Assets/Hill_Check.cs
+++ Assets/Hill_Check.cs
@@ -1,35 +1,32 @@
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Hill_Check : MonoBehaviour
 {
-    // Collision Check with Player
-    // Hill wants 3 stones (always)
-    // Subtracts 3 stones and continues hilling around
-    private void OnCollisionEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object is the player
+        if (collision.collider.CompareTag("Player"))
         {
-            // Check if the colliding object is the player
-            if (other.CompareTag("Player"))
+            Inventory inventory = collision.collider.GetComponent<Inventory>();
+            if (inventory == null)
             {
-                Inventory inventory = other.GetComponent<Inventory>();
-                int num_stones = inventory.GetItemNum("Stone");
-                if (num_stones == null)
-                {
-                    Debug.Log("Player does not have a Stone component!");
-                    return;
-                }
-
-                // Check if the player has at least 3 stones
-                if (num_stones >= 3)
-                {
-                    Debug.Log("Granted");
-                    inventory.SetItemNum("Stone", num_stones - 3); // Subtract 3 stones
-                }
-                else
-                {
-                    Debug.Log("Denied");
-                }
+                Debug.LogWarning("Player does not have an Inventory component!");
+                return;
             }
-        }                
+
+            // Get the number of stones in the inventory
+            int num_stones = inventory.GetItemNum("Stone");
+
+            // Check if the player has at least 3 stones
+            if (num_stones >= 3)
+            {
+                Debug.Log("Access Granted");
+                inventory.SetItemNum("Stone", num_stones - 3); // Subtract 3 stones
+            }
+            else
+            {
+                Debug.Log("Access Denied: Not enough stones!");
+            }
+        }
+    }
 }
